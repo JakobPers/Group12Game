b9d86e78f225f2ab73409d4562ae59eae91618b7
Jakob Persson <jakob.e.persson@gmail.com>
2020-05-07 14:55:12 +0200

    Models added

---
+++ Group12 FPS/Assets/Scripts/GunScript.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    public float fireRate = 0.005f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public AudioSource GunAudio;
+
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f/fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        GunAudio.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Vector3 impact = hit.normal;
+
+            MonsterShot monsterShot = hit.transform.GetComponent<MonsterShot>();
+            if (monsterShot != null)
+            {
+                
+                monsterShot.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ Group12 FPS/Assets/Scripts/MonsterShot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterShot : MonoBehaviour
+{
+
+    public float Health = 100f;
+
+    Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = gameObject.GetComponent<Animator>();
+        Anim.SetFloat("Health", Health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        Health -= damage;
+
+        Anim.SetFloat("Health", Health);
+
+
+    }
+}
