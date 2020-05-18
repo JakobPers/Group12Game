4b8c0baece5f6de579c2c6dde29dd21561fb7309
Anto <mrmutualism@gmail.com>
2020-05-18 20:14:48 +0200

    Added zombie spawnpoints, with buggy zombies that rush only the original player position.

---
+++ Group12 FPS/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] Spawnpoints;
+    public GameObject zombie;
+    private void Start()
+    {
+        Spawnpoints = new GameObject[4];
+        for (int i = 0; i < Spawnpoints.Length; i++)
+        {
+            Spawnpoints[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+            SpawnEnemy();
+    }
+
+    private void SpawnEnemy()
+    {
+        int spawnerID = Random.Range(0, Spawnpoints.Length);
+        Instantiate(zombie, Spawnpoints[spawnerID].transform.position, 
+                            Spawnpoints[spawnerID].transform.rotation);
+    }
+}
